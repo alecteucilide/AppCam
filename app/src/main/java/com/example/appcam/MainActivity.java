6766daa4684813bc32bab2f595ed16b4e882a663
alecteucilide <alecadrianteucilide@gmail.com>
2020-11-06 15:43:44 +0100

    save full foto done

--- app/src/main/java/com/example/appcam/MainActivity.java
+++ app/src/main/java/com/example/appcam/MainActivity.java
@@ -66,9 +66,7 @@ public class MainActivity extends AppCompatActivity {
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
+                Uri photoURI = FileProvider.getUriForFile(getApplicationContext(), BuildConfig.APPLICATION_ID + ".provider", photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
